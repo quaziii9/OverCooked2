c6083a61a756d6438c0f9fcd451a8115f0fa6b25
ogu <ogu590626@gmail.com>
2024-05-11 01:27:53 +0900

    버스/발판애니메이션

---
+++ Assets/02.Scripts/Map/AniTEST.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniTEST : MonoBehaviour
+{
+    
+        // 자식 객체의 애니메이터 컴포넌트를 저장할 배열
+        private Animator[] childAnimators;
+
+    void Start()
+    {
+        // 모든 자식 객체의 애니메이터 컴포넌트를 가져옴
+        childAnimators = GetComponentsInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        // 특정 조건을 만족하면 모든 자식 객체의 애니메이션을 실행시킴
+        if (Input.GetKeyDown(KeyCode.Space)) // 예시로 스페이스바를 누를 때 실행하도록 설정
+        {
+            PlayChildAnimations();
+        }
+    }
+
+    void PlayChildAnimations()
+    {
+        // 모든 자식 객체의 애니메이션을 실행
+     
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bus"))
+        {
+            foreach (Animator animator in childAnimators)
+            {
+                animator.SetTrigger("flip");
+            }
+        }
+    }
+}
