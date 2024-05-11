e17b7c117bb7b6c1ea92dec67c415bcf18824847
Songryan <tjr6255@gmail.com>
2024-05-11 23:45:43 +0900

    Highlight 작업중
    Highlight 작업중

--- Assets/02.Scripts/GameObject/ObjectHighlight.cs
+++ Assets/02.Scripts/GameObject/ObjectHighlight.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class ObjectHighlight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 플레이어 앞에 놓인 오브젝트가 활성화가 가능한 물건인지 확인
+    public bool activeObject = false;
+    // 오브젝트 MeshRenderer에 교체한 Highligt Material 배열
+    [SerializeField] private Material[] hightlightMaterials;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [HideInInspector] public enum ObjectType { Counter, Craft, Return, Station, Sink, Ingredient, Board, Bin, Plate };
+    public ObjectType objectType;
+
+    //public void Activate
 }
