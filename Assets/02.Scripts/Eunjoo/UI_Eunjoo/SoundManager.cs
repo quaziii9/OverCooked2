142f910f90dfa0eff2a39c465e6d33b1319a379e
quaziii9 <162658248+quaziii9@users.noreply.github.com>
2024-05-11 23:45:32 +0900

    은주 IntroScene

---
+++ Assets/02.Scripts/Eunjoo/UI_Eunjoo/SoundManager.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] BGMSquares; //음량 네모네모
+    [SerializeField] private GameObject[] effectSquares;
+
+
+
+    [Header("음량")]
+    public float volumeBGM = 1f;
+    public float volumeEffect = 1f;
+
+    [Header("배경음악")]
+    public AudioSource BgmAudioSource;
+
+    [Header("효과음")]
+    public AudioSource EffectAudioSource;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+    public void upBGM()
+    {
+        volumeBGM += 0.1f;
+        if (volumeBGM >= 1)
+        {
+            volumeBGM = 1f;
+        }
+        //BgmAudioSource.volume = volumeBGM;
+        SetBGMSquares();
+    }
+    public void downBGM()
+    {
+        volumeBGM -= 0.1f;
+        if (volumeBGM <= 0)
+        {
+            volumeBGM = 0;
+        }
+        //BgmAudioSource.volume = volumeBGM;
+        SetBGMSquares();
+    }
+
+    private void SetBGMSquares()
+    {
+        int j = 0;
+        for (float i = 0; i < 1; i += 0.1f)
+        {
+            if (i < volumeBGM)
+            {
+                BGMSquares[j].transform.GetChild(0).gameObject.SetActive(false);
+                BGMSquares[j].transform.GetChild(1).gameObject.SetActive(true);
+            }
+            else
+            {
+                BGMSquares[j].transform.GetChild(0).gameObject.SetActive(true);
+                BGMSquares[j].transform.GetChild(1).gameObject.SetActive(false);
+            }
+            j++;
+        }
+    }
+
+    public void effectUp()
+    {
+        volumeEffect += 0.1f;
+        if (volumeEffect >= 1)
+        {
+            volumeEffect = 1f;
+        }
+        EffectAudioSource.volume = volumeEffect;
+        SetEffectSquares();
+    }
+    public void effectDown()
+    {
+        volumeEffect -= 0.1f;
+        if (volumeEffect <= 0)
+        {
+            volumeEffect = 0;
+        }
+        EffectAudioSource.volume = volumeEffect;
+        SetEffectSquares();
+    }
+
+    private void SetEffectSquares()
+    {
+        int j = 0;
+        for (float i = 0; i < 1; i += 0.1f)
+        {
+            if (i < volumeEffect) 
+            {
+                effectSquares[j].transform.GetChild(0).gameObject.SetActive(false);
+                effectSquares[j].transform.GetChild(1).gameObject.SetActive(true);
+            }
+            else 
+            {
+                effectSquares[j].transform.GetChild(0).gameObject.SetActive(true);
+                effectSquares[j].transform.GetChild(1).gameObject.SetActive(false);
+            }
+            j++;
+        }
+    }
+}
