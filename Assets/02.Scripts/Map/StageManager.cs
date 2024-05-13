d9cd71adac5ba3acbee17e9e059be8d46974b7cc
orinugul <ogu590626@gamil.com>
2024-05-13 18:22:05 +0900

    [Update] 발판 애니메이션/맵씬수정
    발판애니메이션구현

--- Assets/02.Scripts/Map/StageManager.cs
+++ Assets/02.Scripts/Map/StageManager.cs
@@ -6,14 +6,28 @@ public class StageManager : Singleton<StageManager>
 {
     [SerializeField]
     public int[] stages = {3,1,2,3};
-
+    public bool[] unlock;
+    //= new bool[StageManager.Instance.stages.Length];
     public Sprite YellowStar;
 
     void Start()
     {
-        
+        unlock = new bool[StageManager.Instance.stages.Length];
+        Debug.Log(unlock.Length);
+        unlocknode();
     }
 
+    void unlocknode()
+    {
+        for (int i = 0; i < stages.Length; i++)
+        {
+            if (stages[i] > 0)
+            {
+                unlock[i] = true;
+            }
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
