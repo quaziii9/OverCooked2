7552d42b7c64fc300d79994ae2b5a6bb94808333
ogu <ogu590626@gmail.com>
2024-05-21 07:21:01 +0900

    [Update]플레이어 계단오를수있게수정/프로젝트세팅 그레비티수정
    프로젝트세팅에서 그레비티값이 -300으로 되어있는데 기본설정이 -9.1인걸 감안하면 값이 너무 높아서 그레비티 스케일 줄여야할거 같은데 상의가 필요해보임

--- Assets/02.Scripts/Player/PlayerMoveController.cs
+++ Assets/02.Scripts/Player/PlayerMoveController.cs
@@ -9,11 +9,14 @@ public class PlayerMoveController : MonoBehaviour
     [Header("Speed")]
     [SerializeField] public float moveSpeed = 15f;
     [SerializeField] private float turnSpeed = 10f;
-
+    [SerializeField] private float fallMultiplier = 2.5f; // 떨어질 때 속도 증가
     private Vector2 moveInput;
     private bool isDashing = false;
-
     public int puffCount;
+    public bool isGrounded;
+    public bool isOnStairs;
+    public float downforce=1f;
+
 
     void Start()
     {
@@ -45,12 +48,18 @@ public class PlayerMoveController : MonoBehaviour
             }
 
         }
+
+       
     }
 
     void FixedUpdate()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, downforce);
+        isOnStairs = Physics.Raycast(transform.position, Vector3.down, downforce);
+
         if (!isDashing && moveInput.magnitude != 0)
         {
+
             Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y);
             Vector3 movement = moveDir * moveSpeed;
             rb.velocity = movement;
@@ -59,10 +68,45 @@ public class PlayerMoveController : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(moveDir);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
         }
+        // 땅에 닿아있지 않다면 추가적인 중력 적용
+        if (!isGrounded)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        if (isOnStairs&& moveInput.magnitude != 0)//언덕위에서 이동중일때
+        {
+            rb.AddForce(Vector3.down * 5f , ForceMode.Acceleration);
+        }
+        else if(isOnStairs&& moveInput.magnitude == 0)
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
+
 
     public void OnMove(InputValue inputValue)
     {
         moveInput = inputValue.Get<Vector2>();
     }
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * downforce);
+    }
+     void OnCollisionEnter(Collision other)
+    {
+        // Print how many points are colliding with this transform
+        Debug.Log("Points colliding: " + other.contacts.Length);
+
+        // Print the normal of the first point in the collision.
+        Debug.Log("Normal of the first point: " + other.contacts[0].normal);
+
+        // Draw a different colored ray for every normal in the collision
+        foreach (var item in other.contacts)
+        {
+            Debug.DrawRay(item.point, item.normal * 100, Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), 10f);
+        }
+    }
 }
+
