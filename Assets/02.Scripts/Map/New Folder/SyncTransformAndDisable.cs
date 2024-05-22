ca3459bb0b85dab1baf2e67ac6929b76e92824e8
Orinuguri <ogu590626@gmail.com>
2024-05-22 18:04:03 +0900

    [Update] 월드맵타일 프리팹화 완료

---
+++ Assets/02.Scripts/Map/New Folder/SyncTransformAndDisable.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class SyncTransformAndDisable : MonoBehaviour
+{
+    [Header("Assign Arrays of GameObjects")]
+    public GameObject[] AObjects;
+    public GameObject[] BObjects;
+}
