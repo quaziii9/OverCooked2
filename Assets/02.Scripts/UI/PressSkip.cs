e09cef421c631392c11e63f057ab2ad567fc2578
quaziii9 <162658248+quaziii9@users.noreply.github.com>
2024-05-23 16:14:54 +0900

    [Bug fix] mobile 빌드 버그 해결
    https://forum.unity.com/threads/commandinvokationfailure-failed-to-update-android-sdk-package-list.1535458/
    
    Tomas 1856의 workaround1

--- Assets/02.Scripts/UI/PressSkip.cs
+++ Assets/02.Scripts/UI/PressSkip.cs
@@ -16,7 +16,13 @@ public class PressSkip : MonoBehaviour
         Skip();
 
         #if UNITY_ANDROID
-            MobileTouch();
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            UIManager.Instance.RecipeUIOff();
+            SoundManager.Instance.RecipeUIPopOut();
+            fillImage.fillAmount = 0;
+            // UI 비활성화 및 게임 시작
+        }
         #endif
     }
 
@@ -45,45 +51,15 @@ public class PressSkip : MonoBehaviour
         fillImage.fillAmount = Mathf.Clamp(fillImage.fillAmount, 0f, 1f);
     }
 
-
-    private void MobileTouch()
+    public void MobileSkip()
     {
-            bool isTouching = false;
-
-            // 안드로이드 및 터치 입력 처리
-            if (Input.touchCount > 0)
-            {
-                for (int i = 0; i < Input.touchCount; i++)
-                {
-                    Touch touch = Input.GetTouch(i);
-                    if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-                    {
-                        isTouching = true;
-                        break;
-                    }
-                }
-            }
-
-            if (isTouching)
-            {
-                // 스크린을 누르고 있을 때 fillAmount 증가
-                fillImage.fillAmount += fillSpeed * Time.deltaTime;
-
-                if (fillImage.fillAmount >= 1f)
-                {
-                    UIManager.Instance.RecipeUIOff();
-                    SoundManager.Instance.RecipeUIPopOut();
-                    fillImage.fillAmount = 0;
-                    // UI 비활성화 및 게임 시작
-                }
-            }
-            else
-            {
-                // 스크린을 누르고 있지 않을 때 fillAmount 감소
-                fillImage.fillAmount -= fillSpeed * Time.deltaTime;
-            }
-
-            // fillAmount 값 제한 (0과 1 사이로 클램핑)
-            fillImage.fillAmount = Mathf.Clamp(fillImage.fillAmount, 0f, 1f);
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            UIManager.Instance.RecipeUIOff();
+            SoundManager.Instance.RecipeUIPopOut();
+            fillImage.fillAmount = 0;
+            // UI 비활성화 및 게임 시작
         }
+    }
+
 }
